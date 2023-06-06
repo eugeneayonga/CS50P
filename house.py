d9053cfbf2f28059b6617e3c3b850c5542629f42
Eugene Ayonga <eugeneayonga@gmail.com>
2023-06-06 18:59:47 +0300

    Refactored if, else if and else code to use the new syntax - match

--- house.py
+++ house.py
@@ -1,16 +1,32 @@
 name = input("What's your name? ")
 
-if name == "Daenerys" or name == "Aemon" or name == "Viserys":
-    print("House Targaryen")
-elif name == "Jon":
-    print("House Snow")
-elif name == "Arya":
-    print("House Stark")
-elif name == "Tyrion" or name == "Cersei" or name == "Jaime":
-    print("House Lannister")
-elif name == "Stannis":
-    print("House Baratheon")
-elif name == "Samuel":
-    print("House Tarly")
-else:
-    print("Go away, peasant.") # this is the else statement
+match name:
+    case "Daenerys" | "Aemon" | "Viserys":
+        print("House Targaryen")
+    case "Jon":
+        print("House Snow")
+    case "Arya":
+        print("House Stark")
+    case "Tyrion" | "Cersei" | "Jaime":
+        print("House Lannister")
+    case "Stannis":
+        print("House Baratheon")
+    case "Samuel":
+        print("House Tarly")
+    case _:
+        print("Go away, peasant.")
+
+# if name == "Daenerys" or name == "Aemon" or name == "Viserys":
+#     print("House Targaryen")
+# elif name == "Jon":
+#     print("House Snow")
+# elif name == "Arya":
+#     print("House Stark")
+# elif name == "Tyrion" or name == "Cersei" or name == "Jaime":
+#     print("House Lannister")
+# elif name == "Stannis":
+#     print("House Baratheon")
+# elif name == "Samuel":
+#     print("House Tarly")
+# else:
+#     print("Go away, peasant.") # this is the else statement
