fedba7f856ad0b52c9d390c62b83aa851747931b
Eugene Ayonga <eugeneayonga@gmail.com>
2023-06-10 17:08:20 +0300

    REGEX twitter username from the provided url

---
+++ twitter.py
@@ -0,0 +1,7 @@
+import re
+
+url = input("Enter the URL: ").strip()
+
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)$", url, re.IGNORECASE):
+    print(f"Username:", matches.group(1))
+
